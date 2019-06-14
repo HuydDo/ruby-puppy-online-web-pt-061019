8d2f4631eee31727a6f13b048ee84a7e62f1a177
Huy Do <huyddo40@gmail.com>
2019-06-14 21:40:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,11 @@ require 'pry'
 class Dog
   attr_accessor :name
   @@all = []
-
+  @@name = []
   def initialize(name)
     @name = name
     @@all << self
+    @@name << name
   end
 
   def self.clear_all
@@ -13,7 +14,7 @@ class Dog
   end
 
   def self.all
-    @@all.each do |name|
+    @@name.each do |name|
        puts "#{name}"
     end
   end
